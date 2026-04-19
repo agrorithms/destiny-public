8c810f034483a1905beba49327904aca7d49bfe1
Louis Agro <louisagrogit+git@gmail.com>
2026-04-19 13:11:15 -0400

    updated ecosystem scripts

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,6 +15,18 @@ module.exports = {
             cwd: '/home/ubuntu/destiny-farm-finder',
             watch: false,
             env: { NODE_ENV: 'production' }
+        },
+        {
+            name: 'web',
+            script: 'node_modules/.bin/next',
+            args: 'start -H 127.0.0.1 -p 3000',
+            cwd: '/home/ubuntu/destiny-farm-finder'
+        },
+        {
+            name: 'cloudflared',
+            script: 'cloudflared',
+            args: 'tunnel --config /home/ubuntu/.cloudflared/config.yml run',
+            interpreter: 'none'
         }
     ]
 }
